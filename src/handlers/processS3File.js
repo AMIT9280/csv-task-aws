9870371f2519ace8efff8d41efc81474432382b3
Amit Thakkar <amit.thakkar@wickes.co.uk>
2023-07-19 15:27:06 +0530

    Create Csv-Writer

--- src/handlers/processS3File.js
+++ src/handlers/processS3File.js
@@ -1,8 +1,9 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-const ndjson = require('ndjson'); 
+const ndjson = require('ndjson');
 const { v4: uuidv4 } = require('uuid');
 const { Parser } = require('papaparse');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -18,6 +19,8 @@ exports.processS3File = async (event, context) => {
     // Process each record in the NDJSON file
     const parser = ndjson.parse();
     s3ReadStream.pipe(parser);
+
+    const csvData = []
 
     for await (const record of parser) {
       const { name, phoneNumber, dateOfBirth, emailId, country } = record;
@@ -36,7 +39,40 @@ exports.processS3File = async (event, context) => {
         },
       };
       await dynamodb.put(params).promise();
+      csvData.push(params)
     }
+
+    //Convert NDJSON to CSV format
+    const csvString = new Parser({ header: ['UUID', 'NAME', 'PHONE NUMBER', 'DATE OF BIRTH', 'EMAIL ID', 'COUNTRY'] }).parse(csvData).toString();
+
+    // Upload the converted CSV file to the archival folder in S3
+    const timestamp = new Date().getTime();
+    const archiveKey = `archive/Archive_${timestamp}.csv`;
+
+    //create Csv File
+    const csvWriter = createCsvWriter({
+      path: archiveKey,
+      header:[
+        { id: 'uuid', title: 'UUID' },
+        { id: 'name', title: 'NAME' },
+        { id: 'phoneNumber', title: 'PHONE NUMBER' },
+        { id: 'dateOfBirth', title: 'DATE OF BIRTH' },
+        { id: 'emailId', title: 'EMAIL ID' },
+        { id: 'country', title: 'COUNTRY' }
+      ],
+      alwaysQuote: true
+      
+    });
+
+  try{
+    csvWriter.writeRecords(csvString).then(() => {
+      console.log(`File archived: s3://${bucket}/${archiveKey}`);
+      return s3.readFile('data.csv');
+    });
+  }catch(error){
+    console.log('Error while writing CSV file:', error)
+    throw new Error('Error while writing CSV file:')
+  }
 
     console.log('Processing completed.');
 
@@ -52,51 +88,3 @@ exports.processS3File = async (event, context) => {
     };
   }
 };
-exports.archiveS3File = async (event, context) => {
-  try {
-    const { Records: records } = event;
-    const bucket = records[0].s3.bucket.name;
-    const key = records[0].s3.object.key;
-
-    const getObjectParams = {
-      Bucket: bucket,
-      Key: key,
-    };
-
-    const { Body: fileData } = await s3.getObject(getObjectParams).promise();
-
-    const timestamp = new Date().getTime();
-    const archiveKey = `archive/Archive_${timestamp}.csv`;
-
-   //convert fileData to CSV format
-    const csvData = convertToCSV(fileData); 
-
-    // Upload the converted CSV file to the archive folder
-    const putObjectParams = {
-      Bucket: bucket,
-      Key: archiveKey,
-      Body: csvData,
-    };
-    await s3.putObject(putObjectParams).promise();
-
-    console.log(`File archived: s3://${bucket}/${archiveKey}`);
-
-    return {
-      status: 200,
-      body: 'File archived successfully.',
-    };
-  } catch (error) {
-    console.error('Error archiving S3 file:', error);
-    return {
-      status: 500,
-      body: 'Error archiving S3 file.',
-    };
-  }
-};
-// convert fileData to CSV format
-function convertToCSV(fileData) {
-  const parser = new Parser();
-  const { data: csvData } = parser.parse(fileData.toString());
-  console.log(csvData);
-  return csvData.map((row) => row.join(',')).join('\n');
-}
